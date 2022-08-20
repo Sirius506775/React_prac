55bb410aa4678dff13b376e1d09d6f23c571a8f7
sirius506775@gmail.com <sirius506775@gmail.com>
2022-08-20 13:57:47 +0900

    Create new components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import ExpenseItem from './components/ExpenseItem'
+
 function App() {
+
   return (
-    <div>
+    <div> 
       <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
