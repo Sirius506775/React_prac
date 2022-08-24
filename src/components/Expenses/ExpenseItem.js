2962289fb9368f810b3acb27a7c3e2a1791dea8c
dongheon <leedh506775@gmail.com>
2022-08-24 18:01:25 +0900

    Create ExpenseForm

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('Updated!!');
+    console.log(title)
+  };
+
   return (
     // React에서 class는 예약어이기 때문에 속성값을 줄 때는 className을 사용
     <Card className="expense-item"> 
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
   // JSX의 규칙 : JSX 코드 조각마다 반드시 하나의 Root element를 가져야한다.
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './ExpenseForm.css';
+
+const ExpenseForm = () => {
+
+    return <form className="">
+        <div className='new-expense__controls'/>
+    </form>
+};
+
+export default ExpenseForm;
