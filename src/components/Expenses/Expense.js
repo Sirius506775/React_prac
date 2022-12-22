adbd4ce3de808d7449763aeba0e8f44e708cc83a
Dong Heon Lee <Sirius506775@gmail.com>
2022-12-23 03:05:04 +0900

    Feat : create filterdYear

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -11,38 +11,28 @@ const Expense = (props) => {
 
   const filterChangeHandler = selectedYear => {
     //ExpenxseFilter에서 송신받은 데이터를 selectedYear로 받는다.
-
     setFilterdYear(selectedYear); 
     //상태를 변경하는 함수에 송신받은 데이터인 selectedYear
-    
-    // console.log(selectedYear)
   };
+
+  const filteredExpenses = props.items.filter(expense => {
+
+    return expense.date.getFullYear().toString() === filterdYear;
+
+  });
 
 
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filterdYear} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {filteredExpenses.map(expense => 
+        <ExpenseItem 
+        key={expense.id}
+        title={expense.title} 
+        amount={expense.amount} 
+        date={expense.date}/>)
+        }
       </Card>
     </div>
   );
